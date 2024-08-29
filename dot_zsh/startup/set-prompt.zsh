687e6eebb24e1e109c0ec68cc2a062701e152293
David Hand <davidhand@davidhand.com>
2024-08-29 15:51:51 -0400

    Update .zsh/startup/set-prompt.zsh

--- dot_zsh/startup/set-prompt.zsh
+++ dot_zsh/startup/set-prompt.zsh
@@ -31,4 +31,24 @@ source $ZDOTDIR/startup/basic.zsh
 source $ZDOTDIR/startup/identify-machine.zsh
 
 # https://vincent.bernat.ch/en/blog/2019-zsh-async-vcs-info
-#autoload -Uz vcs_info
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' disable cdv cvs tla mtn p4 svk
+
+parse_git_dirty() {
+  git_status="$(git status 2> /dev/null)"
+  [[ "$git_status" =~ "Changes to be committed:" ]] && echo -n "%F{green}·%f"
+  [[ "$git_status" =~ "Changes not staged for commit:" ]] && echo -n "%F{yellow}·%f"
+  [[ "$git_status" =~ "Untracked files:" ]] && echo -n "%F{red}·%f"
+}
+
+setopt prompt_subst
+
+NEWLINE=$'\n'
+
+autoload -Uz vcs_info # enable vcs_info
+precmd () { vcs_info } # always load before displaying the prompt
+zstyle ':vcs_info:git*' formats ' ↣ (%F{254}%b%F{245})' # format $vcs_info_msg_0_
+
+#PS1='%F{254}%n%F{245} ↣ %F{153}%(5~|%-1~/⋯/%3~|%4~)%F{245}${vcs_info_msg_0_} $(parse_git_dirty)$NEWLINE%F{254}$%f '
+
+#PS1='%(?..
